c804d98c8812afc730ea8b99dad7184b4de0864b
Fausto Alonso <fausto.alonso@captalys.com.br>
2022-12-01 18:26:53 -0300

    - Added docstring in the entrypoint function  - Added version badge

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # ddstatd-clj
 
+
+**Latest version**<br>
+[![Clojars Project](https://img.shields.io/clojars/v/org.clojars._fnalonso/ddstatsd-clj.svg)](https://clojars.org/org.clojars._fnalonso/ddstatsd-clj)
+
+
 A Clojure library designed to send custom metrics to datadog through the DogStatsD agent.
 
 ## Usage
--- src/ddstatsd_clj/core.clj
+++ src/ddstatsd_clj/core.clj
@@ -26,6 +26,29 @@
     (format "%s:%d|%s|#%s" metric-name value (get-type-token type) (format-tags tags))))
 
 (defn send-metric
+  "
+    Sends the metric value to dogstatsD.
+
+    **Params**
+   | name | description |
+   | :---- | :---------- |
+   | connection-map | Map containing the host and port which the metric will be sent. |
+   | metric-name | The name of metric, kebab case is converted to snake case in the dd metric panel |
+   | type | Metric type, allowed values: `increment`, `decrement`, `timing`, `gauge` |
+   | value | Integer value for the metric. |
+   | tags | Map containing the tags for the metric |
+
+   Example:
+   ```clojure
+   (def connection-map {:host \"localhost\" :port 8125})
+
+   ;; Metric without any tag
+   (send-metric connection-map \"my_super_metric.count\" \"increment\" 1)
+
+   ;; Metric with some tag
+   (send-metric connection-map \"my_another_super_metric.count\" \"increment\" 10 {:env \"production\"})
+   ```
+  "
   [connection-map metric-name type value & [tags]]
   {:pre [(spec/check-input ::spec/metric-type type)
          (spec/check-input ::spec/connection-map connection-map)
