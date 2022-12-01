38ec5c620104a1227215447b46e4d7111756c3ae
Fausto Alonso <fausto.alonso@captalys.com.br>
2022-12-01 14:19:04 -0300

    Replacing the cond with a case and added the connection-map param to the send-metric function

--- src/ddstatsd_clj/core.clj
+++ src/ddstatsd_clj/core.clj
@@ -1,18 +1,20 @@
 (ns ddstatsd-clj.core
   (:require [ddstatsd-clj.network :refer [send-packet]]
-            [ddstatsd-clj.specs :refer [check-input ::metric-type]])
+            [ddstatsd-clj.specs :as spec])
   (:gen-class))
 
 
 (defn- get-type-token
   [type-name]
-  (cond (or (= type-name "increment") (= type-name "decrement"))  "c"
-        (= type-name "timing") "ms"
-        (= type-name "gauge") "g"
-        :else nil))
-
+  (case  type-name
+      "increment" "c"
+      "decrement" "c"
+      "timing" "ms"
+      "gauge" "g"
+      "default"))
 
 (defn send-metric
-  [metric-name type value]
-  {:pre (check-input ::metric-type type)}
+  [connection-map metric-name type value]
+  {:pre [(spec/check-input ::spec/metric-type type)
+         (spec/check-input ::spec/connection-map connection-map)] }
   (send-packet {:host "localhost" :port 8125} (format "%s:%d|%s" metric-name value (get-type-token type))))
